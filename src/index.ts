14768f1074a31dc92f608b118d1647ce544d11f1
ane1212 <aneee121212@gmail.com>
2026-05-30 15:35:53 +0200

    Estructura y dependencias del proyecto

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+import express from 'express';
+import "dotenv/config";
+const PORT = process.env.PORT || 3000
+
+const app = express();
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Server running 🚀')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
