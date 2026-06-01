83da1c5dee6111667aef5bc25ea6d55ec265b8bd
ane1212 <aneee121212@gmail.com>
2026-06-01 23:26:22 +0200

    Creación de servicio, rutas y controlador de locales

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { userRouter } from "./user.routes";
 import favoriteRouter from "./favorite.routes";
+import { localRouter } from "./local.routes";
 
 export const apiRouter = Router();
 
 apiRouter.use('/user', userRouter);
 apiRouter.use('/favorites', favoriteRouter);
+apiRouter.use('/local', localRouter);
 
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,3 @@
 export * from './user.service'
 export * from './favorite.service'
+export * from './local.service'
